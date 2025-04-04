8ab8f099d7379e71829c13ca7afa6200b4f16716
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-04-04 20:18:56 +0000

    Example of the tests failing

--- TestAdd.java
+++ TestAdd.java
@@ -3,7 +3,8 @@ import junit.framework.*;
 public class TestAdd extends TestCase {
 
    public void testAdd(){
-      int sum = Calculator.add(2,4);
+      //int sum = Calculator.add(2,4);
+      int sum = Calculator.add(2,5); // This is the line that will fail the test
       assertTrue(sum == 6);
    }
 }
